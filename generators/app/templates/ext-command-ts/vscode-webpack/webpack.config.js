826c65f2e2a51624b135790f344a95fa94d713c1
Nikola Hristov <nikola@playform.cloud>
2024-05-07 03:27:45 +0300

    squash!

--- generators/app/templates/ext-command-ts/vscode-webpack/webpack.config.js
+++ generators/app/templates/ext-command-ts/vscode-webpack/webpack.config.js
@@ -5,9 +5,9 @@
 const path = require('path');
 
 //@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-/** @type WebpackConfig */
+
+
 const extensionConfig = {
   target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
